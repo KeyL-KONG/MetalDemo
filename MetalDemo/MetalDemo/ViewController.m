b9059a88fd8bfb7c69a475b557ac06ea730a6c6d
keyl_kong <keyl_kong@163.com>
2021-03-05 17:09:29 +0800

    # 添加Metal-Draw-View样例

--- MetalDemo/MetalDemo/ViewController.m
+++ MetalDemo/MetalDemo/ViewController.m
@@ -7,6 +7,7 @@
 
 #import "ViewController.h"
 #import "CalculateExample.h"
+#import "AAPLViewController.h"
 
 @interface ViewController ()
 
@@ -17,13 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self calculateExample];
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [self renderExample];
+}
 
 - (void)calculateExample {
     CalculateExample *example = [CalculateExample new];
     [example addArraysByMetal];
+}
+
+- (void)renderExample {
+    AAPLViewController *viewController = [[AAPLViewController alloc] init];
+    [self presentViewController:viewController animated:YES completion:nil];
 }
 
 @end
